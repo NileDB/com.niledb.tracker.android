6d7f09881a07e179610cb07cd717f6e08f8a8ba7
Paco Hernández Gómez <paco@hernandezgomez.com>
2018-04-26 11:55:31 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# NileDB Tracker
+
+![logo](logo.png)
+
+Android app for sending tracking information to NileDB platform (i.e. Courier agents location, ...).
+
+![NileDB](niledb-current.png)
+
+### Code of Conduct
+
+Please note that this project is released with a [Contributor Code of Conduct](CODE_OF_CONDUCT.md).
+By contributing to this project (commenting or opening PR/Issues etc) you are agreeing to follow this conduct, so please
+take the time to read it. 
+
+### License
+
+NileDB Tracker is licensed under the GNU General Public License v3.0. See [LICENSE](LICENSE.txt) for details.
+
+Copyright (c) 2018, NileDB, Inc.
+
+[NileDB Tracker License](LICENSE.txt)
---
+++ app/src/main/java/com/niledb/tracker/SettingsActivity.java
@@ -0,0 +1,161 @@
+package com.niledb.tracker;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.support.v7.app.ActionBar;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.view.MenuItem;
+
+import java.util.List;
+
+public class SettingsActivity extends AppCompatPreferenceActivity {
+
+    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
+
+                preference.setSummary(
+                        index >= 0
+                                ? listPreference.getEntries()[index]
+                                : null);
+            }
+            else {
+                preference.setSummary(stringValue);
+            }
+            return true;
+        }
+    };
+
+    private static boolean isXLargeTablet(Context context) {
+        return (context.getResources().getConfiguration().screenLayout
+                & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_XLARGE;
+    }
+
+    private static void bindPreferenceSummaryToValue(Preference preference) {
+        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+
+        sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setupActionBar();
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onIsMultiPane() {
+        return isXLargeTablet(this);
+    }
+
+    @Override
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public void onBuildHeaders(List<Header> target) {
+        loadHeadersFromResource(R.xml.pref_headers, target);
+    }
+
+    protected boolean isValidFragment(String fragmentName) {
+        return PreferenceFragment.class.getName().equals(fragmentName)
+                || ServicePreferenceFragment.class.getName().equals(fragmentName)
+                || ParametersPreferenceFragment.class.getName().equals(fragmentName)
+                || CommunicationsPreferenceFragment.class.getName().equals(fragmentName);
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class ServicePreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_service);
+            setHasOptionsMenu(true);
+
+            bindPreferenceSummaryToValue(findPreference("agent_id"));
+            bindPreferenceSummaryToValue(findPreference("username"));
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            if (id == android.R.id.home) {
+                startActivity(new Intent(getActivity(), SettingsActivity.class));
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class CommunicationsPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_communications);
+            setHasOptionsMenu(true);
+
+            bindPreferenceSummaryToValue(findPreference("protocol"));
+            bindPreferenceSummaryToValue(findPreference("graphql_endpoint"));
+            bindPreferenceSummaryToValue(findPreference("mqtt_endpoint"));
+            bindPreferenceSummaryToValue(findPreference("min_distance"));
+            bindPreferenceSummaryToValue(findPreference("min_periodicity"));
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            if (id == android.R.id.home) {
+                startActivity(new Intent(getActivity(), SettingsActivity.class));
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class ParametersPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_parameters);
+            setHasOptionsMenu(true);
+
+            bindPreferenceSummaryToValue(findPreference("entity_name"));
+            bindPreferenceSummaryToValue(findPreference("location_attribute_name"));
+            bindPreferenceSummaryToValue(findPreference("altitude_attribute_name"));
+            bindPreferenceSummaryToValue(findPreference("accuracy_attribute_name"));
+            bindPreferenceSummaryToValue(findPreference("agent_id_attribute_name"));
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            if (id == android.R.id.home) {
+                startActivity(new Intent(getActivity(), SettingsActivity.class));
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+    }
+}

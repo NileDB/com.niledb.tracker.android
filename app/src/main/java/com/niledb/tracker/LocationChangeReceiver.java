ca3cac85eaaa483fdefe0aed1ebd8205c703fb6c
Paco Hernández Gómez <paco@hernandezgomez.com>
2018-04-26 19:38:08 +0200

    Commit.

---
+++ app/src/main/java/com/niledb/tracker/LocationChangeReceiver.java
@@ -0,0 +1,56 @@
+package com.niledb.tracker;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import java.util.Date;
+
+public class LocationChangeReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getExtras() != null) {
+            Location location = (Location) intent.getExtras().get(LocationManager.KEY_LOCATION_CHANGED);
+            if (location != null) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                double altitude = location.getAltitude();
+                float accuracy = location.getAccuracy();
+
+                Log.i("NileDB", latitude + ", " + longitude + ", " + altitude + ", " + accuracy);
+
+                boolean serviceEnabled = PreferenceManager.getDefaultSharedPreferences(context).getBoolean("service_enabled", false);
+                int agentId = Integer.parseInt(PreferenceManager.getDefaultSharedPreferences(context).getString("agent_id", "1"));
+                boolean authenticationRequired = PreferenceManager.getDefaultSharedPreferences(context).getBoolean("authentication_required", false);
+                String username = PreferenceManager.getDefaultSharedPreferences(context).getString("username", null);
+                String password = PreferenceManager.getDefaultSharedPreferences(context).getString("password", null);
+                String adminPassword = PreferenceManager.getDefaultSharedPreferences(context).getString("admin_password", null);
+
+                int protocol = Integer.parseInt(PreferenceManager.getDefaultSharedPreferences(context).getString("protocol", "0"));
+                String graphqlEndpoint = PreferenceManager.getDefaultSharedPreferences(context).getString("graphql_endpoint", "https://niledb.com/graphql");
+                String mqttEndpoint = PreferenceManager.getDefaultSharedPreferences(context).getString("mqtt_endpoint", "https://niledb.com:1883");
+                int minDistance = Integer.parseInt(PreferenceManager.getDefaultSharedPreferences(context).getString("min_distance", "500"));
+                int minPeriodicity = Integer.parseInt(PreferenceManager.getDefaultSharedPreferences(context).getString("min_periodicity", "60000"));
+
+                String entityName = PreferenceManager.getDefaultSharedPreferences(context).getString("entity_name", "CarrierAgentLocation");
+                String agentIdParameterName = PreferenceManager.getDefaultSharedPreferences(context).getString("agent_id_attribute_name", "carrierAgent");
+                String locationParameterName = PreferenceManager.getDefaultSharedPreferences(context).getString("location_attribute_name", "location");
+                String altitudeParameterName = PreferenceManager.getDefaultSharedPreferences(context).getString("altitude_attribute_name", null);
+                String accuracyParameterName = PreferenceManager.getDefaultSharedPreferences(context).getString("accuracy_attribute_name", null);
+
+                Log.i("NileDB", new Date() + "," + serviceEnabled + ", " + agentId + ", " + authenticationRequired + ", " + username + ", " + password + ", " + adminPassword + ", " + protocol + ", " + graphqlEndpoint + ", " + mqttEndpoint + ", " + minDistance + ", " + minPeriodicity + ", " + entityName + ", " + agentIdParameterName + ", " + locationParameterName + ", " + altitudeParameterName + ", " + accuracyParameterName);
+
+                ToneGenerator toneGenerator = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+                toneGenerator.startTone(ToneGenerator.TONE_CDMA_PIP, 150);
+
+            }
+        }
+    }
+}
--- app/src/main/java/com/niledb/tracker/SettingsActivity.java
+++ app/src/main/java/com/niledb/tracker/SettingsActivity.java
@@ -1,21 +1,38 @@
 package com.niledb.tracker;
 
+import android.Manifest;
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.List;
 
 public class SettingsActivity extends AppCompatPreferenceActivity {
+
+    static LocationManager locationManager = null;
+    static PendingIntent launchIntent = null;
+
+    static Context applicationContext = null;
+    static Activity activity = null;
 
     private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
         @Override
@@ -32,6 +49,18 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                                 : null);
             }
             else {
+                if (preference.getKey().equals("agent_id")) {
+                    try {
+                        int agentId = Integer.parseInt(stringValue);
+                        if (agentId <= 0 || !stringValue.equals("" + agentId)) {
+                            throw new NumberFormatException();
+                        }
+                    }
+                    catch(NumberFormatException e) {
+                        Toast.makeText(applicationContext, "Please, enter a positive integer without trailing 0's", Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                }
                 preference.setSummary(stringValue);
             }
             return true;
@@ -53,9 +82,53 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 0) {
+            enableOrDisableService();
+        }
+    }
+
+    static void enableOrDisableService() {
+        if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, new String[] { Manifest.permission.ACCESS_FINE_LOCATION }, 0 );
+        }
+
+        boolean serviceEnabled = PreferenceManager.getDefaultSharedPreferences(applicationContext).getBoolean("service_enabled", false);
+
+        if (serviceEnabled) {
+            if (ContextCompat.checkSelfPermission(applicationContext, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                int minDistance = Integer.parseInt(PreferenceManager.getDefaultSharedPreferences(applicationContext).getString("min_distance", "500"));
+                int minPeriodicity = Integer.parseInt(PreferenceManager.getDefaultSharedPreferences(applicationContext).getString("min_periodicity", "60000"));
+                //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minPeriodicity, minDistance, launchIntent);
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 10, launchIntent);
+            }
+        }
+        else {
+            locationManager.removeUpdates(launchIntent);
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setupActionBar();
+
+        if (applicationContext == null) {
+            applicationContext = getApplicationContext();
+        }
+
+        if (activity == null) {
+            activity = this;
+        }
+
+        if (locationManager == null) {
+            locationManager = (LocationManager) applicationContext.getSystemService(Context.LOCATION_SERVICE);
+        }
+        if (launchIntent == null) {
+            Intent intent = new Intent("NILEDB_TRACKER_SEND_LOCATION_ACTION");
+            launchIntent = PendingIntent.getBroadcast(applicationContext, 0, intent, 0);
+        }
+        enableOrDisableService();
     }
 
     private void setupActionBar() {
@@ -85,7 +158,9 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
     }
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public static class ServicePreferenceFragment extends PreferenceFragment {
+    public static class ServicePreferenceFragment extends PreferenceFragment
+            implements SharedPreferences.OnSharedPreferenceChangeListener {
+
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -105,6 +180,26 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             }
             return super.onOptionsItemSelected(item);
         }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            if (key.equals("service_enabled")) {
+                enableOrDisableService();
+            }
+        }
+
     }
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
